2a4b277db5794f24f4b378604d8ef6354234b800
Keith Chin <bankit@gmail.com>
2010-07-02 09:53:37 +0800

    adding in unfinished files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
 
+  @title = "entrée"
   private
     def current_user_session
       return @current_user_session if defined?(@current_user_session)
